024ac14022756b371c8f4e5ec8c484268ede113b
MidnightPavlaka <156126701+MidnightPavlaka635920@users.noreply.github.com>
2026-01-10 11:44:30 +0100

    Delete install.h

--- install.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <nlohmann/json.hpp>
-#include "curl_access.h"
-using json = nlohmann::json;
-
-void install_mod(const std::string& pn, const std::string& install_path, const json& req, bool just_install);
