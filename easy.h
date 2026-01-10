8c43746c1a0380b185674a2c51a3602df1cb072d
MidnightPavlaka <156126701+MidnightPavlaka635920@users.noreply.github.com>
2026-01-10 11:44:15 +0100

    Delete easy.h

--- easy.h
+++
@@ -1,8 +0,0 @@
-#pragma once 
-#include <iostream>
-#include <string>
-#include <nlohmann/json.hpp>
-#include <vector>
-using json = nlohmann::json;
-void easy_install(std::string& install_path);
-void easy_remove(std::string& install_path);
