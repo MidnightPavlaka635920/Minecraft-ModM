c5dbe02a09b31194e978877241760bafbc0c4276
MidnightPavlaka <156126701+MidnightPavlaka635920@users.noreply.github.com>
2026-03-23 20:08:11 +0100

    Add MSYS64 update step to install script
    Added update message and command for MSYS64.

--- installwin.sh
+++ installwin.sh
@@ -2,6 +2,8 @@ if [[ "$MSYSTEM" != "MINGW64" ]]; then
     echo "Please run this in the MINGW64 shell (not MSYS or CMD)"
     exit 1
 fi
+echo "Updating MSYS64, please wait..."
+pacman -Syu -y
 pacman -Sy -y mingw-w64-x86_64-toolchain make mingw-w64-x86_64-nlohmann-json mingw-w64-x86_64-curl
 echo "Dependencies installed. Now building and installing mcpk..."
 make
