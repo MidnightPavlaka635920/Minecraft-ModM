8c6a09599048399d2bd3d83a5cf4a21bbc8b7e56
MidnightPavlaka <156126701+MidnightPavlaka635920@users.noreply.github.com>
2026-03-23 20:10:30 +0100

    Update installwin.sh to install specific dependencies
    Changed pacman command to only update the package database and install specific dependencies.

--- installwin.sh
+++ installwin.sh
@@ -3,7 +3,6 @@ if [[ "$MSYSTEM" != "MINGW64" ]]; then
     exit 1
 fi
 echo "Updating MSYS64, please wait..."
-pacman -Syu -y
 pacman -Sy -y mingw-w64-x86_64-toolchain make mingw-w64-x86_64-nlohmann-json mingw-w64-x86_64-curl
 echo "Dependencies installed. Now building and installing mcpk..."
 make
