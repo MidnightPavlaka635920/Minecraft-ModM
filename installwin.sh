5217d2adac2d3737b93e2e4e60176f9c64cb1e5c
MidnightPavlaka <156126701+MidnightPavlaka635920@users.noreply.github.com>
2026-03-23 20:05:41 +0100

    Added installwin file for Windows

---
+++ installwin.sh
@@ -0,0 +1,9 @@
+if [[ "$MSYSTEM" != "MINGW64" ]]; then
+    echo "Please run this in the MINGW64 shell (not MSYS or CMD)"
+    exit 1
+fi
+pacman -Sy -y mingw-w64-x86_64-toolchain make mingw-w64-x86_64-nlohmann-json mingw-w64-x86_64-curl
+echo "Dependencies installed. Now building and installing mcpk..."
+make
+echo "Running installation..."
+make installwin
