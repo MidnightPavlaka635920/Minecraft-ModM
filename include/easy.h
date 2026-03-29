c08fbe6db60af56f49e4957f7e206e5e715575b5
MidnightPavlaka <156126701+MidnightPavlaka635920@users.noreply.github.com>
2026-03-29 18:15:16 +0200

    Fixed compiler bug in easy.h

--- include/easy.h
+++ include/easy.h
@@ -5,6 +5,6 @@
 #include <vector>
 using json = nlohmann::json;
 void easy_install(std::string& install_path, bool color);
-void easy_remove(std::string& install_path, bool color;
+void easy_remove(std::string& install_path, bool color);
 __attribute__((used))
 const char a_thing[] = "In case you are wondering who is in 450479388_496240406415295_5860082617347840614_n.jpg, take a look at the source of my last project. It would be clear ;)";
